a3d774fa05e4f08c4948c972d1310c584a373e61
Zach-Adolphsen <zladolphsen@gmail.com>
2025-03-17 10:52:05 -0500

    adding component interactions

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   imports: [],
   templateUrl: './child.component.html',
-  styleUrl: './child.component.css'
+  styleUrl: './child.component.css',
 })
 export class ChildComponent {
+  @Input() inputText: string = '';
+  @Output() outputText: EventEmitter<string> = new EventEmitter<string>();
+  message: string = '';
 
+  sendMessage(value: string) {
+    this.message = value;
+    this.outputText.emit(this.message);
+  }
 }
